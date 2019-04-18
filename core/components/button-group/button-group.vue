e8d032b9dd7379697b0afa565f8ecc515884ddd1
张军 <819396183@qq.com>
2019-04-18 10:56:57 +0800

    修改lint的警告

--- core/components/button-group/button-group.vue
+++ core/components/button-group/button-group.vue
@@ -5,41 +5,41 @@
 </template>
 
 <script>
-    import FormDomSizeMixin from '../../mixins/form-dom-size-mixin'
-    let prefix = 'zov-button-group'
-    export default {
-        name: prefix,
-        mixins: [FormDomSizeMixin],
-        props: {
-            // 形状有2种，default、circle
-            shape: {
-                type: String,
-                validator (value) {
-                    return ['default', 'circle'].indexOf(value) !== -1
-                },
-                default: 'default'
+import FormDomSizeMixin from '../../mixins/form-dom-size-mixin'
+let prefix = 'zov-button-group'
+export default {
+    name: prefix,
+    mixins: [FormDomSizeMixin],
+    props: {
+        // 形状有2种，default、circle
+        shape: {
+            type: String,
+            validator (value) {
+                return ['default', 'circle'].indexOf(value) !== -1
             },
-            vertical: {
-                type: Boolean,
-                default: false
-            }
+            default: 'default'
         },
-        data () {
-            return {
-                stylePrefix: prefix
-            }
-        },
-        computed: {
-            classes () {
-                return [
-                    this.stylePrefix,
-                    this.sizeClasses,
-                    {
-                        [this.stylePrefix + '-' + this.shape]: !!this.shape,
-                        [this.stylePrefix + '-vertical']: this.vertical
-                    }
-                    ]
-            }
+        vertical: {
+            type: Boolean,
+            default: false
+        }
+    },
+    data () {
+        return {
+            stylePrefix: prefix
+        }
+    },
+    computed: {
+        classes () {
+            return [
+                this.stylePrefix,
+                this.sizeClasses,
+                {
+                    [this.stylePrefix + '-' + this.shape]: !!this.shape,
+                    [this.stylePrefix + '-vertical']: this.vertical
+                }
+            ]
         }
     }
+}
 </script>
