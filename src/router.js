e05f7e337456e03ef965bcdea2b47fec082e4da1
张军 <819396183@qq.com>
2019-04-24 18:56:27 +0800

    style(layout): 样式修改

--- src/router.js
+++ src/router.js
@@ -1,21 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Zov from '../core'
+import Home from './Home'
+import Menu from './Menu'
 Vue.use(Router)
 let routerObject = {
     routes: [
         {
             path: '/',
-            component: () => import('./Home')
+            component: Home
         },
         {
             path: '/menu',
-            component: () => import('./Menu'),
+            component: Menu,
             children: []
         },
         {
             path: '/component',
-            component: () => import('./Menu'),
+            component: Menu,
             children: []
         }
     ]
@@ -27,10 +29,9 @@ requireViews.keys().forEach(fileName => {
     let options = requireViews(fileName).default.routerOptions
     console.log(options)
     let componentName = fileName.substr(2, fileName.length - 10)
-    fileName = fileName.substr(0, 1) + '/views' + fileName.substr(1)
     let o = {
         path: '/' + componentName,
-        component: () => import(fileName + ''),
+        component: requireViews(fileName).default,
         routerOptions: options
     }
     viewsRoutes.push(o)
@@ -44,10 +45,9 @@ routerObject.routes[1].children = routerObject.routes[1].children.concat(viewsRo
 const requireComponents = require.context('./views/components', false, /\.vue/)
 requireComponents.keys().forEach(fileName => {
     let componentName = fileName.substr(2, fileName.length - 10)
-    fileName = fileName.substr(0, 1) + '/views/components' + fileName.substr(1)
     routerObject.routes[2].children.push({
         path: '/' + componentName,
-        component: () => import(fileName + '')
+        component: requireComponents(fileName).default
     })
 })
 console.log(routerObject)
