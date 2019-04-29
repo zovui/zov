8471dbc913b48454d286cc1073557d90f4d71cde
longshihui <391424924@qq.com>
2019-04-29 21:37:12 +0800

    chore: 新增prettier格式化工具

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,18 @@
+/** @format */
+
 module.exports = {
-    root: true,
-    env: {
-        node: true
-    },
-    'extends': [
-        'plugin:vue/essential',
-        '@vue/standard'
-    ],
-    rules: {
-        'indent': ['error', 4],
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-        'no-useless-return': 'off'
-    },
-    parserOptions: {
-        parser: 'babel-eslint'
-    }
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/standard', '@vue/prettier'],
+	rules: {
+		indent: ['error', 4],
+		'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+		'no-useless-return': 'off'
+	},
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 }
