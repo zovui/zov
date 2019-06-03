bbe97a180e83deda0b7e68313c5e11c6eadc35cc
liyi17 <liyi17@58.com>
2019-06-03 17:48:57 +0800

    fix: 修复了divider颜色 应用主题色

--- core/components/divider/divider.vue
+++ core/components/divider/divider.vue
@@ -1,79 +1,69 @@
 <template>
-    <div :class="classes" :style="styles">
-        <span :class="txtClass" v-if="hasTxt">
-            <slot></slot>
-        </span>
-    </div>
+	<div :class="classes">
+		<span :class="txtClass" v-if="hasTxt">
+			<slot></slot>
+		</span>
+	</div>
 </template>
 <script>
-    const prefix = 'zov-divider';
-    export default {
-        name:prefix,
-        data(){
-            return {
-                stylePrefix: prefix,
-                txtClass:`${prefix}-inner-text`,
-                hasTxt:false,
-            }
-        },
-        props:{
-            className:{
-                type:String,
-                default:''
-            },
-            dashed:{
-                type:Boolean,
-                default:false
-            },
-            //分割线标题位置，分为三种：center、left、right
-            orientation:{
-                type:String,
-                default:'center',
-                validator:function(value){
-                    if(['center','left','right'].indexOf(value) !== -1){
-                        return value;
-                    }
-                    return 'center';
-                }
-            },
-            styles:{
-                type:Object,
-                default:function(){
-                    return {}
-                },
-                validator:function(value){
-                    if(typeof value === 'object'){
-                        return value;
-                    }
-                    return {}
-                }
-            },
-            type:{
-                type:String,
-                default:'horizontal',
-                validator:function(value){
-                    if(['horizontal','vertical'].indexOf(value) !== -1){
-                        return value;
-                    }
-                    return 'horizontal';
-                }
-            }
-        },
-        computed:{
-            classes:function(){
-                var hasTxt = !!this.$slots.default;
-                this.hasTxt = hasTxt;
-                return [
-                    this.stylePrefix,
-                    (this.stylePrefix + '-' + this.type),
-                    (this.dashed ? (this.stylePrefix + '-dashed') : ''),
-                    hasTxt ? (this.stylePrefix + '-with-text-' + this.orientation) : '',
-                    this.className
-                ]
-            }
-        }
-    }
+const prefix = 'zov-divider'
+export default {
+	name: prefix,
+	data() {
+		return {
+			stylePrefix: prefix,
+			txtClass: `${prefix}-inner-text`,
+			hasTxt: false
+		}
+	},
+	props: {
+		dashed: {
+			//是否虚线
+			type: Boolean,
+			default: false
+		},
+		dotted: {
+			//是否点状线
+			type: Boolean,
+			default: false
+		},
+		//分割线标题位置，分为三种：center、left、right
+		orientation: {
+			type: String,
+			default: 'center',
+			validator: function(value) {
+				if (['center', 'left', 'right'].indexOf(value) !== -1) {
+					return value
+				}
+				return 'center'
+			}
+		},
+		type: {
+			type: String,
+			default: 'horizontal',
+			validator: function(value) {
+				if (['horizontal', 'vertical'].indexOf(value) !== -1) {
+					return value
+				}
+				return 'horizontal'
+			}
+		}
+	},
+	computed: {
+		classes: function() {
+			var hasTxt = !!this.$slots.default
+			this.hasTxt = hasTxt
+			return [
+				this.stylePrefix,
+				this.stylePrefix + '-' + this.type,
+				this.dashed ? this.stylePrefix + '-dashed' : '',
+				this.dotted ? this.stylePrefix + '-dotted' : '',
+				hasTxt
+					? this.stylePrefix + '-with-text-' + this.orientation
+					: ''
+			]
+		}
+	}
+}
 </script>
-<style>
-
-</style>
+<style></style>
